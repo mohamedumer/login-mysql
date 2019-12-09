4da41355c5a30ccd527df7601459267767531d6a
mohamedumer <53818424+mohamedumer@users.noreply.github.com>
2019-12-09 20:26:54 +0530

    Delete local.js

--- src/local.js
+++
@@ -1,4 +0,0 @@
-const file= require("fs");
-var object={a:10}
-var da=JSON.stringify(obj);
-file.writeFile("./localdata.json",da,(err)=>{if(err){console.log("error")}else{console.log("successfully written")}})
